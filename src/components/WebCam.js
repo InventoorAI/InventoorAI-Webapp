8ac3d274a594e8592772a7b3e8f8bfba96b33af6
Cwjiee <weijie@oyencov.com>
2024-06-08 20:25:00 +0800

    feat: webcam works!!!!!

--- src/components/WebCam.js
+++ src/components/WebCam.js
@@ -1,10 +1,28 @@
 import { Camera } from "lucide-react";
 import WidgetLayout from "./WidgetLayout";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 export default function WebCam({ ...style }) {
+  useEffect(() => {
+    const socket = io('ws://192.168.145.251:3000')
+
+    socket.on('mjpeg-stream', (chunk) => {
+      const img = document.getElementById('mjpeg-stream');
+      console.log('chunk', chunk);
+
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        img.src = reader.result;
+      };
+      reader.readAsDataURL(new Blob([chunk]));
+    });
+  }, [])
+
   return (
     <WidgetLayout title={"Web Cam"} icon={<Camera />} {...style}>
-      <img src="http://192.168.31.60/webcam/stream" />
+      <img id="mjpeg-stream" width="400px" height="250px" />
     </WidgetLayout>
   );
 }
